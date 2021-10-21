73c2873339a3c22580d681591337deac2d276555
dhindakiflia <dhindakeep@gmail.com>
2021-10-21 17:59:36 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+
+    <title>Seternak</title>
+  </head>
+  <body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark hijau shadow">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="assets/logop.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ">
+            <a class="mx-4 nav-link active " aria-current="page" href="#">Seternak</a>
+            <a class="mx-4 nav-link " href="#">Tahukah Kamu</a>
+            <a class="mx-4 nav-link " href="#">Tentang Kami</a>
+        </div>
+        </div>
+    </div>
+</nav>
+<!-- Navbarend -->
+
+
+<!-- Jumbotron -->
+<section class="jumbotron" style="padding-top:150px;padding-bottom:100px" class="img-fluid">
+  <h1 class="display-2 text-light bold fw-bold text-center" class="responsive-font-example">Seternak</h1>
+  <p class="lead text-center text-light">Aplikasi yang mengintegrasikan kegiatan peternak dengan fitur mentoring, forum, marketplace, dan info</p> 
+  <div class="d-flex justify-content-center ">
+        <button type="button" class="btn btn-light mx-1 text-success">Daftar</button>
+        <button type="button" class="btn btn-success mx-1 hijau">Masuk</button>
+  </div>
+</section>
+<!-- Jumbotronend -->
+
+<!-- Tujuan Seternak -->
+<section id="tujuan">
+    <div class="container me-3 py-5">
+    
+
+        <div class="row justify-content-center ms-1">
+            <div class="col-8 py-3 ms-5" >
+                <h2>Tujuan Seternak</h2>
+                <p class="py-3">Memberikan pemahaman baru kepada peternak dari pakar ahli
+                    Memungkinkan semua peternak dapat terhubung secara realtime
+                    Efektifitas proses sosialisasi peternak kepada khalayak terkait produktifitas kerja
+                    Membantu peternak dalam memasarkan hasil ternak desa secara lebih luas</p>
+            </div>
+            <div class="col ms-4">
+            <img src="assets/ayam.jpg" style="width:115px; height:163px;">
+            
+            </div>
+        </div>    
+    </div>
+</section>
+<!-- Tujuanseternakend -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
