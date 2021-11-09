355d25fba8456dba2dc64457563180cc64232281
annisasuprima <annisa.suprima@gmail.com>
2021-11-09 21:51:55 +0700

    ahli dan profile

--- layout/admin-footer.php
+++ layout/admin-footer.php
@@ -1,18 +1,28 @@
+<style>
+ @media (max-width: 600px) {
+      .footer-wrapper {
+        flex-direction: row;
+        margin-bottom: unset;
+
+      }
+
+    }
+</style>
+
 <footer id="footer" class="hijau">
         <br><br>
         <div class="container-md">
             <div class="row text-light">
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <img src="assets/logop.jpg" alt="" style="width:40px;height:40px">
                 </div>
-                <div id="ifoot" class="col-sm-2 pt-3 text-end">Seternak</div>
-                <div id="ifoot" class="col-sm-2 pt-3 text-end">Tahukah Kamu</div>
-                <div id="ifoot" class="col-sm-2 pt-3 text-end">Tentang Kami
-                    <a href="#"></a>
-                </div>
+                <div id="ifoot" class="col-sm-2 pt-3 text-end">Ahli</div>
+                <div id="ifoot" class="col-sm-2 pt-3 text-end">Diskusi</div>
+                <div id="ifoot" class="col-sm-2 pt-3 text-end">Informasi</div>
+                <div id="ifoot" class="col-sm-2 pt-3 text-end">Manage User</div>
             </div>
             <hr class="my-2 text-light">
-            <div class="row text-light mb-2">
+            <div class="row text-light mb-2 footer-wrapper">
                 <div id="ifoot" class="col-md-10">© 2021 Ambyar (Anak Muda Berbudaya dan Berkarya)</div>
                 <div id="ifoot" class="col-md pt-1 text-end" ><a href="http://"><i class="bi bi-facebook text-light"></i></a></div>
                 <div id="ifoot" class="col-md pt-1 text-end"><a href="http://"><i class="bi bi-instagram text-light"></i></a></div>
